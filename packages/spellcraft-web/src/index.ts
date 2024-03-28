549d3fa92af531fc3672d0daf3c8df9536f3ede3
Anthony Bonta <zthun.coding@gmail.com>
2024-03-28 11:53:50 -0500

    refactor: descriptions

--- packages/spellcraft-web/src/index.ts
+++ packages/spellcraft-web/src/index.ts
@@ -1,8 +1,12 @@
 import { includeCustomElement } from '@zthun/spellcraft';
 import { ZSpellcraftCardElement } from './card/card-element.mjs';
+import { ZSpellcraftCodeBlockElement } from './code-block/code-block-element.mjs';
 import { ZSpellcraftHeaderElement } from './header/header-element.mjs';
 import { ZSpellcraftThemeElement } from './theme/spellcraft-theme-element.mjs';
+
+import 'highlight.js/styles/github-dark.css';
 
 includeCustomElement(ZSpellcraftThemeElement);
 includeCustomElement(ZSpellcraftHeaderElement);
 includeCustomElement(ZSpellcraftCardElement);
+includeCustomElement(ZSpellcraftCodeBlockElement);
