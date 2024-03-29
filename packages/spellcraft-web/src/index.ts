839212e4b7eb303832261d76d266e3e381dbc659
Anthony Bonta <zthun.coding@gmail.com>
2024-03-29 14:17:19 -0500

    refactor: we don't actually need an include method

--- packages/spellcraft-web/src/index.ts
+++ packages/spellcraft-web/src/index.ts
@@ -1,14 +1,7 @@
-import { includeCustomElement } from '@zthun/spellcraft';
-import { ZSpellcraftCardElement } from './card/card-element.mjs';
-import { ZSpellcraftCodeBlockElement } from './code-block/code-block-element.mjs';
-import { ZSpellcraftHeaderElement } from './header/header-element.mjs';
-import { ZSpellcraftThemeElement } from './theme/spellcraft-theme-element.mjs';
-
 import 'highlight.js/styles/github-dark.css';
-import { ZSpellcraftIconElement } from './icon/icon-element.mjs';
 
-includeCustomElement(ZSpellcraftThemeElement);
-includeCustomElement(ZSpellcraftHeaderElement);
-includeCustomElement(ZSpellcraftCardElement);
-includeCustomElement(ZSpellcraftCodeBlockElement);
-includeCustomElement(ZSpellcraftIconElement);
+import './card/card-element.mjs';
+import './code-block/code-block-element.mjs';
+import './header/header-element.mjs';
+import './icon/icon-element.mjs';
+import './theme/spellcraft-theme-element.mjs';
