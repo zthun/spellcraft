52da3135f56c96c89092453dc3ee76d0569908f7
Anthony Bonta <zthun.coding@gmail.com>
2024-04-01 11:42:49 -0500

    fix: render on attribute change moved the element type declaration up

--- packages/spellcraft/src/component/component-render-on-attribute-change.mts
+++ packages/spellcraft/src/component/component-render-on-attribute-change.mts
@@ -1,7 +1,8 @@
 import { IZLifecycleAttributeChanged } from '../lifecycle/lifecycle-attribute-changed.mjs';
+import { ZComponentConstructor } from './component-constructor.mjs';
 
 /**
- * A mixin decorator that adds a render invocation on the attribute changed
+ * An aspect that adds a render invocation on the attribute changed
  * lifecycle event.
  *
  * In order for the component to actually render anything, it needs to implement
@@ -10,6 +11,9 @@ import { IZLifecycleAttributeChanged } from '../lifecycle/lifecycle-attribute-ch
  *
  * The target node for the render method will be the shadowRoot if it is set, or
  * will point to this object if there is no shadow root.
+ *
+ * @param TElement -
+ *        The element type that this aspect decorates.
  *
  * @returns
  *        A new decorated type that automatically implements {@link IZLifecycleAttributeChanged}.
@@ -27,16 +31,16 @@ import { IZLifecycleAttributeChanged } from '../lifecycle/lifecycle-attribute-ch
  * }
  * ```
  */
-export function ZComponentRenderOnAttributeChanged() {
-  return function <C extends typeof HTMLElement>(Target: C) {
+export function ZComponentRenderOnAttributeChanged<TElement extends HTMLElement>() {
+  return function (Target: ZComponentConstructor<TElement>): any {
     const _Target = Target as any;
 
-    const _ZComponentRenderOnAttributeChanged: any = class extends _Target implements IZLifecycleAttributeChanged {
+    class _ZComponentRenderOnAttributeChanged extends _Target implements IZLifecycleAttributeChanged {
       public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
         super.attributeChangedCallback?.call(this, name, oldValue, newValue);
         this.render?.call(this, this.shadowRoot || this);
       }
-    };
+    }
 
     return _ZComponentRenderOnAttributeChanged;
   };
--- packages/spellcraft/src/component/component-render-on-attribute-change.spec.ts
+++ packages/spellcraft/src/component/component-render-on-attribute-change.spec.ts
@@ -1,4 +1,5 @@
 import { createGuid, html } from '@zthun/helpful-fn';
+import { IZLifecycleAttributeChanged } from 'src/lifecycle/lifecycle-attribute-changed.mjs';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 import { ZAttribute } from '../attribute/attribute.mjs';
 import { ZNode } from '../node/node.mjs';
@@ -11,35 +12,54 @@ describe('ZComponentRenderOnAttributeChange', () => {
     new ZNode(document.body).clear();
   });
 
-  const createTestTarget = <T extends HTMLElement>($tag: string) => {
-    const $html = html`<div><${$tag}></${$tag}></div>`;
-    const template = document.createElement('template');
-    template.innerHTML = $html;
-    document.body.appendChild(template.content.cloneNode(true));
-    return document.body.querySelector<T>($tag)!;
-  };
-
   describe('Custom Element Flow', () => {
     const tag = 'z-component-render-custom-element-test';
 
     @ZComponentRegister(tag)
     @ZComponentRenderOnAttributeChanged()
-    class ZComponentRenderCustomElementTest extends HTMLElement implements IZComponentRender {
+    class ZComponentRenderCustomElementTest
+      extends HTMLElement
+      implements IZComponentRender, IZLifecycleAttributeChanged
+    {
       public static readonly observedAttributes = ['identity'];
 
       @ZAttribute()
       public identity: string;
 
       public render = vi.fn();
+      public _attributeChangedCallback = vi.fn();
+
+      public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+        return this._attributeChangedCallback(name, oldValue, newValue);
+      }
     }
+
+    const createTestTarget = () => {
+      const $html = html`<div><${tag}></${tag}></div>`;
+      const template = document.createElement('template');
+      template.innerHTML = $html;
+      document.body.appendChild(template.content.cloneNode(true));
+      return document.body.querySelector<ZComponentRenderCustomElementTest>(tag)!;
+    };
 
     it('should render when an attribute changes', () => {
       // Arrange.
-      const target = createTestTarget<ZComponentRenderCustomElementTest>(tag);
+      const target = createTestTarget();
       // Act.
       target.identity = createGuid();
       // Assert.
       expect(target.render).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call the parent attribute changed callback', () => {
+      // Arrange.
+      const expected = createGuid();
+      const target = createTestTarget();
+      // Act.
+      target.identity = expected;
+      // Assert.
+      expect(target._attributeChangedCallback).toHaveBeenCalledTimes(1);
+      expect(target._attributeChangedCallback).toHaveBeenCalledWith('identity', null, expected);
     });
   });
 
@@ -56,6 +76,12 @@ describe('ZComponentRenderOnAttributeChange', () => {
       public identity: string;
 
       public render = vi.fn();
+
+      public _attributeChangedCallback = vi.fn();
+
+      public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+        return this._attributeChangedCallback(name, oldValue, newValue);
+      }
     }
 
     const createTestTarget = () => {
@@ -73,6 +99,17 @@ describe('ZComponentRenderOnAttributeChange', () => {
       target.identity = createGuid();
       // Assert.
       expect(target.render).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call the parent attribute changed callback', () => {
+      // Arrange.
+      const expected = createGuid();
+      const target = createTestTarget();
+      // Act.
+      target.identity = expected;
+      // Assert.
+      expect(target._attributeChangedCallback).toHaveBeenCalledTimes(1);
+      expect(target._attributeChangedCallback).toHaveBeenCalledWith('identity', null, expected);
     });
   });
 });
