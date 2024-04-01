fd10bca5b4287b75971840479e0f0e573ac9ba26
Anthony Bonta <zthun.coding@gmail.com>
2024-04-01 15:00:36 -0500

    build: allow unsafe merge declarations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,3 @@
-module.exports = require('@zthun/lint-janitor-config/eslint');
+const config = require('@zthun/lint-janitor-config/eslint');
+config.rules['@typescript-eslint/no-unsafe-declaration-merging'] = 'off';
+module.exports = config;
