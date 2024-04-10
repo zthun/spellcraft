a82d61137b49781013cda5af4684dd90f9adefa1
Anthony Bonta <zthun.coding@gmail.com>
2024-04-10 14:52:45 -0500

    feat: template no display adds a template that renders a style with display none and important
    Also supports the slot.

---
+++ packages/spellcraft/src/template/component-template-no-display.mts
@@ -0,0 +1,40 @@
+import { html } from '@zthun/helpful-fn';
+import { ZComponentConstructor } from '../component/component-constructor.mjs';
+import { IZComponentTemplate } from '../render/component-render-template.mjs';
+
+/**
+ * Requirements for ZComponentRenderTemplate targets
+ */
+export type ZComponentTemplateNoDisplayRequirements = HTMLElement;
+
+/**
+ * An aspect that extends a component and adds a template member that basically amounts to
+ * display: none with a slot.
+ *
+ * This will normally be used in background components which are only responsible for
+ * supplying values to the host component.
+ *
+ * @param T -
+ *        The type of element that the decorator extends.
+ *
+ * @returns
+ *        A new decorated type that automatically implements a render method that clears
+ *        the target shadow root or target node and renders an html template.
+ */
+export function ZComponentTemplateNoDisplay<T extends ZComponentTemplateNoDisplayRequirements>() {
+  return function (Target: ZComponentConstructor<T>): any {
+    // @ts-expect-error https://github.com/microsoft/TypeScript/issues/58022
+    return class _ZComponentTemplateNoDisplay extends Target implements IZComponentTemplate {
+      public template() {
+        return html`
+          <style>
+            :host {
+              display: none !important;
+            }
+          </style>
+          <slot></slot>
+        `;
+      }
+    };
+  };
+}
---
+++ packages/spellcraft/src/template/component-template-no-display.spec.ts
@@ -0,0 +1,42 @@
+import { html } from '@zthun/helpful-fn';
+import { afterEach, describe, expect, it } from 'vitest';
+import { ZComponentRegister } from '../component/component-register.mjs';
+import { ZComponentShadow } from '../component/component-shadow.mjs';
+import { ZNode } from '../node/node.mjs';
+import { ZComponentRenderOnConnected } from '../render/component-render-on-connected.mjs';
+import { IZComponentTemplate, ZComponentRenderTemplate } from '../render/component-render-template.mjs';
+import { IZComponentRender } from '../render/component-render.mjs';
+import { ZComponentTemplateNoDisplay } from './component-template-no-display.mjs';
+
+describe('ZComponentTemplateNoDisplay', () => {
+  const tag = 'z-component-template-no-display-test';
+
+  afterEach(() => {
+    new ZNode(document.body).clear();
+  });
+
+  interface ZComponentRenderWithTemplateTest extends IZComponentTemplate, IZComponentRender {}
+
+  @ZComponentRegister(tag)
+  @ZComponentRenderOnConnected()
+  @ZComponentRenderTemplate()
+  @ZComponentTemplateNoDisplay()
+  @ZComponentShadow()
+  class ZComponentRenderWithTemplateTest extends HTMLElement {}
+
+  const createTestTarget = () => {
+    const template = document.createElement('template');
+    template.innerHTML = html`<div><${tag}></${tag}></div>`;
+    document.body.appendChild(template.content.cloneNode(true));
+    return document.body.querySelector<ZComponentRenderWithTemplateTest>(tag)!;
+  };
+
+  it('should not display the component', () => {
+    // Arrange.
+    const target = createTestTarget();
+    // Act.
+    const actual = target.shadowRoot?.querySelector('style')?.textContent;
+    // Assert.
+    expect(actual).toContain('display: none');
+  });
+});
