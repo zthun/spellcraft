0599c48d942563a97124fc84fd64a48c86e2aa4c
Anthony Bonta <zthun.coding@gmail.com>
2024-04-11 09:21:14 -0500

    test: template slot allows for testing where we just need to pass through some other component

---
+++ packages/spellcraft/src/template/component-template-slot.mts
@@ -0,0 +1,32 @@
+import { html } from '@zthun/helpful-fn';
+import { ZComponentConstructor } from '../component/component-constructor.mjs';
+import { IZComponentTemplate } from '../render/component-render-template.mjs';
+
+/**
+ * Requirements for ZComponentTemplateSlot targets
+ */
+export type ZComponentTemplateSlotRequirements = HTMLElement;
+
+/**
+ * An aspect that extends a component and adds a template member that adds a slot
+ * component.
+ *
+ * This is mostly useful for testing.
+ *
+ * @param T -
+ *        The type of element that the decorator extends.
+ *
+ * @returns
+ *        A new decorated type that automatically implements a render method that clears
+ *        the target shadow root or target node and renders an html template.
+ */
+export function ZComponentTemplateSlot<T extends ZComponentTemplateSlotRequirements>() {
+  return function (Target: ZComponentConstructor<T>): any {
+    // @ts-expect-error https://github.com/microsoft/TypeScript/issues/58022
+    return class _ZComponentTemplateSlot extends Target implements IZComponentTemplate {
+      public template() {
+        return html`<slot></slot>`;
+      }
+    };
+  };
+}
---
+++ packages/spellcraft/src/template/component-template-slot.spec.ts
@@ -0,0 +1,48 @@
+import { html } from '@zthun/helpful-fn';
+import { afterEach, describe, expect, it } from 'vitest';
+import { ZComponentRegister } from '../component/component-register.mjs';
+import { ZComponentShadow } from '../component/component-shadow.mjs';
+import { ZNode } from '../node/node.mjs';
+import { ZComponentRenderOnConnected } from '../render/component-render-on-connected.mjs';
+import { IZComponentTemplate, ZComponentRenderTemplate } from '../render/component-render-template.mjs';
+import { IZComponentRender } from '../render/component-render.mjs';
+import { ZComponentTemplateSlot } from './component-template-slot.mjs';
+
+describe('ZComponentTemplateSlot', () => {
+  const tag = 'z-component-template-slot-test';
+
+  afterEach(() => {
+    new ZNode(document.body).clear();
+  });
+
+  interface ZComponentTemplateSlotTest extends IZComponentTemplate, IZComponentRender {}
+
+  @ZComponentRegister(tag)
+  @ZComponentRenderOnConnected()
+  @ZComponentRenderTemplate()
+  @ZComponentTemplateSlot()
+  @ZComponentShadow()
+  class ZComponentTemplateSlotTest extends HTMLElement {}
+
+  const createTestTarget = () => {
+    const template = document.createElement('template');
+    template.innerHTML = html`
+      <div>
+        <${tag}>
+          <div>Slotted</div>
+        </${tag}>
+      </div>
+    `;
+    document.body.appendChild(template.content.cloneNode(true));
+    return document.body.querySelector<ZComponentTemplateSlotTest>(tag)!;
+  };
+
+  it('should slot the content', () => {
+    // Arrange.
+    const target = createTestTarget();
+    // Act.
+    const actual = target.textContent;
+    // Assert.
+    expect(actual).toContain('Slotted');
+  });
+});
