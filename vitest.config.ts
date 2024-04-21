2f3bde8fd831de4a713e07173189e44dbffb8cfb
Anthony Bonta <zthun.coding@gmail.com>
2024-04-21 15:32:43 -0500

    test: upgrade to use workspaces

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,7 @@
-import tsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
   test: {
-    environment: 'jsdom',
-    testTimeout: 30000,
     coverage: {
       all: false,
       provider: 'istanbul'
