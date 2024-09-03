a360f1e098ddbaf86722c2e87da14254985acd39
Anthony Bonta <zthun.coding@gmail.com>
2024-09-02 20:58:58 -0500

    chore: upgrade lint-janitor-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
 const config = require("@zthun/lint-janitor-config/eslint");
-config.rules["@typescript-eslint/no-unsafe-declaration-merging"] = "off";
-config.rules["@typescript-eslint/no-empty-object-type"] = "off";
 config.env = { node: true, browser: true };
 module.exports = config;
